1dd647d46d11b6cae65708c9ecdc9efd3e4bfb6a
Maxim Bogochunas <bogochunas@gmail.com>
2017-07-09 21:33:14 +0300

    added crawlers config file

--- catalog/controller/extension/module/company.php
+++ catalog/controller/extension/module/company.php
@@ -4,12 +4,6 @@
  *
  * @package Stock distribution
  * @version 0.0.7
- * 
- * @source admin/view/javascript/advertikon/advertikon.js
- * @#source admin/view/javascript/advertikon/company/*
- * 
- * @#source admin/view/stylesheet/advertikon/company/*
- * @source admin/view/stylesheet/advertikon/advertikon.css
  */
 
 class ControllerExtensionModuleCompany extends Controller {
